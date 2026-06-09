f6f0e1c7df8450faef6faec7b5b11f63f8e28ce9
azanabdullah2752012 <azanabdullah27.5.2012@gmail.com>
2026-06-09 20:15:45 +0530

    Updates

--- assets/check-BvX-_crK.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./authService-3kJ5ktTf.js";var t=e(`check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
---
+++ assets/chevron-right-C_WTov32.js
@@ -0,0 +1,1 @@
+import{r as e}from"./authService-CxLARrnv.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
